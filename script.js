298553210cd1423dd5be7a74f5432a81d8283fa2
Petrus <petrusheya@gmail.com>
2023-12-19 22:08:01 +0200

    Added features section

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const middleNav = document.querySelector('.middle-nav');
-const middleNavContent = document.querySelector('p,a');
 const topNav = document.querySelector('.top-nav');
 const menuBtn = document.querySelector('.top-nav svg:nth-child(2)');
 let isMenuOpen = false;
 
 function openMenu(){
     if (isMenuOpen === false){
-        middleNavContent.style.opacity = '1';
         topNav.style.borderRadius = '20px 20px 0px 0px';
         menuBtn.style.transform = 'rotate(90deg)';
-        middleNav.style.transform = 'scaleY(1)';
+        middleNav.style.height = '350px';
+        middleNav.style.padding = '20px 30px';
+        middleNav.style.border = '1px solid var(--border-color)'
         isMenuOpen = true;
         console.log(isMenuOpen);
     }
     else if (isMenuOpen === true){
-        middleNavContent.style.opacity = '0';
-        middleNav.style.transform = 'scaleY(0)';
-        topNav.style.borderRadius = '20px 20px 20px 20px';
+        middleNav.style.height = '0';
+        middleNav.style.padding = '0';
+        middleNav.style.border = 'none';
         menuBtn.style.transform = 'rotate(45deg';
+        topNav.style.borderRadius = '20px 20px 20px 20px';
         isMenuOpen = false;
         console.log(isMenuOpen);
     }
