b5eef6d9eabe79dd0bb0dbc699c7d736ae992c1d
Petrus <petrusheya@gmail.com>
2023-12-27 19:51:46 +0200

    Remade the nav bar

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const middleNav = document.querySelector('.middle-nav');
-const topNav = document.querySelector('.top-nav');
+const navWrapper = document.querySelector('.nav-wrapper');
 const menuBtn = document.querySelector('.top-nav svg:nth-child(2)');
 let isMenuOpen = false;
 
 function openMenu(){
-    if (isMenuOpen === false){
-        topNav.style.borderRadius = '20px 20px 0px 0px';
+    if (!isMenuOpen){
         menuBtn.style.transform = 'rotate(90deg)';
-        middleNav.style.height = '350px';
-        middleNav.style.padding = '20px 30px';
-        middleNav.style.border = '1px solid var(--border-color)'
+        navWrapper.style.height = '390px';
         isMenuOpen = true;
-        console.log(isMenuOpen);
     }
-    else if (isMenuOpen === true){
-        middleNav.style.height = '0';
-        middleNav.style.padding = '0';
-        middleNav.style.border = 'none';
+    else if (isMenuOpen){
+        navWrapper.style.height = '60px';
         menuBtn.style.transform = 'rotate(45deg';
-        topNav.style.borderRadius = '20px 20px 20px 20px';
         isMenuOpen = false;
-        console.log(isMenuOpen);
     }
 }
